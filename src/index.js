66aa86769fc440009cb8b503c1461b626ef64cc2
Hwan <hwan.shin@gmail.com>
2020-09-09 16:40:52 +0900

    implement note app

--- src/index.js
+++ src/index.js
@@ -2,47 +2,50 @@
 // This is the main entry point of our application
 const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4002;
 
-const typeDefs = gql`
-    type Query {
-        hello: String
-    }
-`;
-
-const resolvers = {
-    Query: {
-        hello: () => 'Hello world!'
-    }
-};
-
-type Pizza {
-    id: ID!
-    size: String!
-    slices: Int!
-    toppings: [String]
-}
-
-type Note {
-    id: ID!
-    content: String!
-    auther: String!
-}
-
-type Query {
-    hello: String!
-    notes: [Note!]!
-}
-
-Query: {
-    hello: () => 'Hello world!',
-    notes: () => notes
-}
 let notes = [
     { id: '1', content:'This is a note', auther:'Adam Scott' },
     { id: '2', content: 'This is another note', auther: 'Harlow Everly'},
     { id: '3', content: 'Oh hey look, another note', auther: 'Riley Harrison'}
 ];
+
+const typeDefs = gql`
+    type Note {
+        id: ID!
+        content: String!
+        auther: String!
+    }
+    type Query {
+        hello: String!
+        notes: [Note!]!
+        note(id:ID!): Note!
+    }
+    type Mutation {
+        newNote(content: String!): Note!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!',
+        notes: () => notes,
+        note: (parent, args) => {
+            return notes.find(note => note.id === args.id);
+        }
+    }, 
+    Mutation: {
+        newNote: (parent, args) => {
+            let noteValue = {
+                id: String(notes.length+1),
+                content: args.content,
+                author: "Adam"
+            };
+            notes.push(noteValue);
+            return noteValue;
+        }
+    }
+};
 
 const app = express();
 
